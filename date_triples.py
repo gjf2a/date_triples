91e95c0f053b2d48a390f1a4f003c34783e12373
gjf2a <ferrer@hendrix.edu>
2020-12-16 14:40:08 -0600

    Initial commit.

---
+++ date_triples.py
@@ -0,0 +1,16 @@
+import calendar
+
+
+def pythagorean_triple(a, b, c):
+    return a ** 2 + b ** 2 == c ** 2
+
+
+def pythagorean_dates(start_year, end_year):
+    dates = []
+    c = calendar.Calendar()
+    for year in range(start_year, end_year + 1):
+        for month in range(1, 13):
+            for day in (d for d in c.itermonthdays(year, month) if d != 0):
+                if pythagorean_triple(day, month, year % 100):
+                    dates.append((day, month, year))
+    return dates
